0bad1ed32bc3468ca9c1a744fcb4889c403bfac4
Alen Miskovic <92217202+alenmiskovic@users.noreply.github.com>
2023-09-18 14:29:51 +0200

    nastavak

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,71 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <img src="@/assets/putovanje.jpg" alt="" width="40" height="40" class="d-inline-block align-text-top">
-          Plan Putovanja
-        </a>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <nav id="nav" class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+              <a class="navbar-brand" href="#">
+                
+  <img src="@/assets/putovanje.jpg" alt="¸Putovanje" width="50" height="50" class="d-inline-block align-text-top">                                
+              </a>
+              <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                  <router-link to="/" class="nav-link">Home</router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link to="/login" class="nav-link">Prijava</router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link to="/signup" class="nav-link">Registracija</router-link>
+                </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Pretraga" aria-label="Search">
+              </form>
+            </div>
+          </nav>
+        </div>
       </div>
-    </nav>
-    <div class="nav-links">
-      <router-link to="/">Home</router-link>
-      <router-link to="/login">Prijava</router-link>
-      <router-link to="/signup">Registracija</router-link>
     </div>
-    <router-view/>
+
+    <div class="container">
+      <router-view/>
+      <!-- tu router dinamicki ubaci screen -->
+    </div>
+
   </div>
 </template>
 
-<style>
-.nav-links {
-  display: flex; 
-  justify-content: space-around; 
-  background-color: #f8f9fa; 
-  padding: 10px; 
-}
-.nav-links router-link {
-  text-decoration: none; 
-  padding: 10px;
-}
-</style>
-
-
-
-
 <style lang="scss">
+body {
+  background-color: rgba(var(--b3f,250,250,250),1);
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+  background-color: white;
+  border-bottom: #ccc 1px solid;
+
+  margin-bottom: 30px;
+  a {
+    font-weight: bold;
+    color: lightcoral;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-view">
+    <div class="container">
+      <h1>Isplanirajte svoje putovanje</h1>
+      <p>Ovo je vaše mjesto za planiranje i praćenje putovanja.</p>
+
+      <!-- Dodajte ostale komponente i sadržaj ovdje -->
+
+      <router-link to="/destinacije" class="btn btn-primary">Pregledaj destinacije</router-link>
+    </div>
   </div>
 </template>
+
 
 <script>
 // @ is an alias to /src
