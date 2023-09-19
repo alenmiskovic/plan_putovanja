b3a01837e13822c9a371e95bf623f9f6af67231c
alenmiskovic <92217202+alenmiskovic@users.noreply.github.com>
2023-09-20 00:04:15 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
       <h1>Isplanirajte svoje putovanje</h1>
       <p>Ovo je vaše mjesto za planiranje i praćenje putovanja.</p>
 
-      <!-- Dodajte ostale komponente i sadržaj ovdje -->
+      
 
       <router-link to="/destinacije" class="btn btn-primary">Pregledaj destinacije</router-link>
     </div>
